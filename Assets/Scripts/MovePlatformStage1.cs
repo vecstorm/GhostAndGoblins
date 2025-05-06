819a28dc563a4860360a32e484d489718c3012c8
vecstorm <a24vicmilsal@inspedralbes.cat>
2025-05-06 17:01:43 +0200

    Movimiento de la platafroma de la stage 1

---
+++ Assets/Scripts/MovePlatformStage1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatformStage1 : MonoBehaviour
+{
+    public GameObject[] waypoints;
+
+    public float platformSpeed = 2;
+
+    private int waypointsIndex = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform()
+    {
+        if (Vector2.Distance(transform.position, waypoints[waypointsIndex].transform.position) < 0.1f)
+        {
+            waypointsIndex++;
+
+            if (waypointsIndex >= waypoints.Length)
+            {
+                waypointsIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointsIndex].transform.position, platformSpeed * Time.deltaTime);
+
+    }
+
+}
