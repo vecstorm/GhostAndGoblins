576e588ac329ac0b8d68e14e123d1b08aacf7b2b
IrisFrades <a24irifragra@inspedralbes.cat>
2025-05-15 15:41:25 +0200

    Scripts de Json creados

---
+++ Assets/JSON/SaveGameData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveGameData : MonoBehaviour
+{
+    public static PlayerInfoSerialized gameData;
+
+    public static void SaveDataInfo(PlayerInfoSerialized game)
+    {
+        string json = JsonUtility.ToJson(game);
+        File.WriteAllText(Application.persistentDataPath + "GameData.txt", json);
+        Debug.Log(Application.persistentDataPath);
+        Debug.Log("Partida guardada en la BBDD");
+    }
+}
